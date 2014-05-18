eb80df3b1817abb9985ef77b793bab7cd8870453
Wade Brainerd <wadetb@gmail.com>
2014-05-18 12:50:34 -0400

    Plugin simplified, new functionality added.
    Header row is now a selectable option when sorting, not a per file setting.  I didn't like how it was stored in the User's preferences.
    Ascending vs. Descending sort is also selected when sorting, not a separate command.
    Sorting attempts to sort numerically before falling back to text sort.
    Added Insert column and Delete column features.
    
    Notable TODO items:
    + The code which detects the selected column doesn't handle quoted values in the CSV table, this is not good.
    + After reformatting the document, the cursor position should be restored to the cell that was selected.
    + Some syntax for embedding cells to be evaluated.

--- csvplugin.py
+++ csvplugin.py
@@ -1,15 +1,42 @@
-# Written by Eric Martel (emartel@gmail.com / www.ericmartel.com)
+# Originally written by Eric Martel (emartel@gmail.com / www.ericmartel.com)
+# Improved by Wade Brainerd (wadetb@gmail.com / www.wadeb.com)
+
+# TODO
+# + GetColumnIndexFromCursor doesn't handle quotes etc.
+# + Restore selection after operation.
 
 import sublime
 import sublime_plugin
 
-import threading
-import os
-import json
-
 class SortDirection:
     Ascending = 1
     Descending = 2
+
+class CSVValue:
+    def __init__(self, text):
+        self.text = text
+
+    def AsFloat(self):
+        try:
+            return True, float(self.text)
+        except ValueError:
+            return False, None
+
+    def Compare(self, other):
+        a_is_float, a_float = self.AsFloat()
+        b_is_float, b_float = other.AsFloat()
+
+        if a_is_float and b_is_float:
+            return a_float - b_float
+
+        if self.text > other.text:
+            return 1
+        if self.text < other.text:
+            return -1
+        return 0
+
+    def __lt__(self, other): return self.Compare(other) < 0
+    def __eq__(self, other): return self.Compare(other) == 0
 
 class CSVMatrix:
     def __init__(self):
@@ -32,73 +59,145 @@ class CSVMatrix:
 
         self.valid = True
 
-    def SortByColumn(self, column_index, sortdirection):
-        if sortdirection == SortDirection.Ascending:
-            self.rows.sort(key=lambda row: row[column_index])
-        else:
-            self.rows.sort(key=lambda row: row[column_index], reverse=True)
+    def GetCellValue(row, column_index):
+        try:
+            return row[column_index]
+        except IndexError:
+            return CSVValue('')
 
-    def FormatCompact(self, trailing_delimiters):
+    def SortByColumn(self, column_index, direction, use_header):
+        class Compare:
+            def __init__(self, row): self.value = CSVMatrix.GetCellValue(row, column_index)
+            def __lt__(self, other): return self.value < other.value
+            def __eq__(self, other): return self.value == other.value
+
+        reverse = direction == SortDirection.Descending
+
+        if use_header:
+            self.rows[1:] = sorted(self.rows[1:], key=lambda row: Compare(row), reverse=reverse)
+        else:
+            self.rows.sort(key=lambda row: Compare(row), reverse=reverse)
+
+    def InsertColumn(self, column_index):
+        for row in self.rows:
+            if column_index <= len(row):
+                row.insert(column_index, CSVValue(''))
+
+    def DeleteColumn(self, column_index):
+        for row in self.rows:
+            if column_index < len(row):
+                row.pop(column_index)
+
+    def QuoteText(self, text):
+        if self.delimiter in text or '"' in text:
+            return '"' + text.replace('"', '""') + '"'
+        else:
+            return text
+
+    def GetColumnWidths(self):
+        column_widths = [0] * self.num_columns
+
+        for row in self.rows:
+            for column_index, value in enumerate(row):
+                text = self.QuoteText(value.text)
+                width = len(text)
+
+                if width > column_widths[column_index]:
+                    column_widths[column_index] = width
+
+        return column_widths
+
+    def Format(self):
         output = ''
 
         for row in self.rows:
-            for column_index in range(self.num_columns):
-                if column_index < len(row):
-                    column = row[column_index]
+            row_text = ''
 
-                    if column.find(self.delimiter) != -1 or column.find('"') != -1:
-                        output += '"'
-                        output += column.replace('"', '""')
-                        output += '"'
+            for column_index, value in enumerate(row):
+                quoted_text = self.QuoteText(value.text)
 
-                    else:
-                        output += column
+                row_text += quoted_text
 
-                if column_index < len(row) - 1 or trailing_delimiters:
-                    output += self.delimiter
+                if column_index < len(row) - 1:
+                    row_text += self.delimiter
 
-            output += '\n'
+            output += row_text + '\n'
+
+        return output
+
+    def FormatCompacted(self):
+        output = ''
+
+        for row in self.rows:
+            row_text = ''
+
+            for column_index, value in enumerate(row):
+                quoted_trimmed_text = self.QuoteText(value.text).strip()
+
+                row_text += quoted_trimmed_text
+
+                if column_index < len(row) - 1:
+                    row_text += self.delimiter
+
+            output += row_text + '\n'
+
+        return output
+
+    def FormatExpanded(self):
+        column_widths = self.GetColumnWidths()
+
+        output = ''
+
+        for row in self.rows:
+            row_text = ''
+
+            for column_index, value in enumerate(row):
+                quoted_text = self.QuoteText(value.text)
+
+                column_width = column_widths[column_index]
+
+                quoted_padded_text = quoted_text.ljust(column_width)
+
+                row_text += quoted_padded_text
+
+                if column_index < len(row) - 1:
+                    row_text += self.delimiter
+
+            output += row_text + '\n'
 
         return output
 
     # not done through regex for clarity and control
     # not done using csv module to have better control over what happens with the quotes
-    def ParseRow(self, row, trim_whitespace):
+    def ParseRow(self, row):
         columns = []
         currentword = ''
         insidequotes = False
-        pending_double_quote = False
 
-        for char in row:
+        for char_index, char in enumerate(row):
+            if char == '"':
+                if char_index < len(row) - 1 and row[char_index + 1] == '"':
+                    currentword += '"'
+                    continue
+
             if insidequotes:
                 if char == '"':
-                    if pending_double_quote:
-                        currentword += '"'
                     insidequotes = False
-
                 else:
                     currentword += char
-                    pending_double_quote = False
 
             else:
                 if char == '"':
                     insidequotes = True
-                    pending_double_quote = True
 
                 elif char == self.delimiter:
-                    if trim_whitespace:
-                        currentword = currentword.strip()
-                    columns.append(currentword)
+                    columns.append(CSVValue(currentword))
                     currentword = ''
-                    continue
 
                 else:
                     currentword += char
 
-        # add the last word
-        if trim_whitespace:
-            currentword = currentword.strip()
-        columns.append(currentword)
+        columns.append(CSVValue(currentword))
 
         return columns
 
@@ -107,7 +206,7 @@ class CSVMatrix:
 
         for line in text.split("\n"):
             if len(line.strip()):
-                row = matrix.ParseRow(line, True)
+                row = matrix.ParseRow(line)
 
                 matrix.AddRow(row)
 
@@ -137,73 +236,65 @@ class CsvSetOutputCommand(sublime_plugin.TextCommand):
         if(args['output'] != None):
             self.view.replace(edit, sublime.Region(0, self.view.size()), args['output']);
 
-class CsvSortAscCurrentColCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        matrix = CSVMatrix.FromView(self.view)
-        if not matrix.valid:
-            sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
-            return
-
-        column_index = GetColumnIndexFromCursor(self.view)
-
-        matrix.SortByColumn(column_index, SortDirection.Ascending)
-
-        output = matrix.FormatCompact(False)
-        self.view.replace(edit, sublime.Region(0, self.view.size()), output);
-
-class CsvSortDescCurrentColCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        matrix = CSVMatrix.FromView(self.view)
-        if not matrix.valid:
-            sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
-            return
-
-        column_index = GetColumnIndexFromCursor(self.view)
-
-        matrix.SortByColumn(column_index, SortDirection.Descending)
-        
-        output = matrix.FormatCompact(False)
-        self.view.replace(edit, sublime.Region(0, self.view.size()), output);
-
-class CsvSortAscPromptColCommand(sublime_plugin.WindowCommand):
+class CsvSortByColCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.view = self.window.active_view()
+
         self.matrix = CSVMatrix.FromView(self.view)
         if not self.matrix.valid:
             sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
             return
 
-        self.window.show_quick_panel(self.matrix.rows[0], self.on_select_done)
+        self.column_index = GetColumnIndexFromCursor(self.view)
 
-    def on_select_done(self, picked):
+        self.window.show_quick_panel(['Ascending', 'Descending'], self.on_select_direction_done)
+
+    def on_select_direction_done(self, picked):
         if picked >= 0:
-            column_index = picked
+            directions = [SortDirection.Ascending, SortDirection.Descending]
+            self.direction = directions[picked]
 
-            self.matrix.SortByColumn(column_index, SortDirection.Ascending)
+            sublime.set_timeout(lambda: self.window.show_quick_panel(['Use header row', 'Don\'t use header row'], self.on_select_header_done), 10)
+
+    def on_select_header_done(self, picked):
+        if picked >= 0:
+            use_header = picked == 0
+
+            self.matrix.SortByColumn(self.column_index, self.direction, use_header)
             
-            output = self.matrix.FormatCompact(False)
+            output = self.matrix.Format()
 
             self.view.run_command('csv_set_output', {'output': output});
 
-class CsvSortDescPromptColCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.view = self.window.active_view()
-        self.matrix = CSVMatrix.FromView(self.view)
-        if not self.matrix.valid:
+class CsvInsertColCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        matrix = CSVMatrix.FromView(self.view)
+        if not matrix.valid:
             sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
             return
 
-        self.window.show_quick_panel(self.matrix.rows[0], self.on_select_done)
+        column_index = GetColumnIndexFromCursor(self.view)
 
-    def on_select_done(self, picked):
-        if picked >= 0:
-            column_index = picked
+        matrix.InsertColumn(column_index)
 
-            self.matrix.SortByColumn(column_index, SortDirection.Descending)
-           
-            output = self.matrix.FormatCompact(False)
+        output = matrix.Format()
 
-            self.view.run_command('csv_set_output', {'output': output});
+        self.view.replace(edit, sublime.Region(0, self.view.size()), output);
+
+class CsvDeleteColCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        matrix = CSVMatrix.FromView(self.view)
+        if not matrix.valid:
+            sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
+            return
+
+        column_index = GetColumnIndexFromCursor(self.view)
+
+        matrix.DeleteColumn(column_index)
+
+        output = matrix.Format()
+
+        self.view.replace(edit, sublime.Region(0, self.view.size()), output);
 
 class CsvFormatCompactCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -213,6 +304,18 @@ class CsvFormatCompactCommand(sublime_plugin.WindowCommand):
             sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
             return
 
-        output = matrix.FormatCompact(False)
+        output = matrix.FormatCompacted()
+
+        view.run_command('csv_set_output', {'output': output});
+
+class CsvFormatExpandCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view()
+        matrix = CSVMatrix.FromView(view)
+        if not matrix.valid:
+            sublime.error_message(__name__ + ": The buffer doesn't appear to be a CSV file")
+            return
+
+        output = matrix.FormatExpanded()
 
         view.run_command('csv_set_output', {'output': output});
